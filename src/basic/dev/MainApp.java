37258c94deab18f95fc1e335c5b3cd938e732a83
nhattan22399 <linhatan99@gmail.com>
2021-04-22 16:50:35 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,27 @@
+package basic.dev;
+/**
+ * 
+ * @author Admin
+ *
+ */
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Khai báo biến
+		 */
+		String firstName ="Phan van";
+		String lastName ="Nam";
+		int old = 15;
+		float markMath = 9f;
+		boolean passed = true;
+		System.out.println("Ho va ten");
+		System.out.println(firstName + " "+ lastName);
+		System.out.println("markMath "+ markMath);
+		System.out.println("passed  "+ passed);
+		//khai báo Hằng. Hằng phải viết hoa
+		final float PI = 3.14f;
+	}
+
+}
